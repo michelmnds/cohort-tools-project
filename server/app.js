9bd8bf10be04fb76221aaaf91c8d4ba309de54ca
michelmnds <m1tchofc@gmail.com>
2024-01-17 13:41:13 +0000

    added routes for students and cohorts

--- server/app.js
+++ server/app.js
@@ -39,30 +39,81 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-//  GET  /students - Retrieve all books from the database
-app.get("/api/students", (req, res) => {
-  Student.find({})
-    .then((students) => {
-      console.log("Retrieved students ->", students);
-      res.json(students);
-    })
-    .catch((error) => {
-      console.error("Error while retrieving students ->", error);
-      res.status(500).send({ error: "Failed to retrieve students" });
-    });
+//  GET  /students
+app.get("/api/students", async (req, res) => {
+  try {
+    const students = await Student.find();
+    console.log("Retrieved students ->", students);
+    res.json(students);
+  } catch (error) {
+    console.error("Error while retrieving students ->", error);
+    res.status(500).send({ error: "Failed to retrieve students" });
+  }
+});
+app.get("/api/students/:studentId", async (req, res) => {
+  const studentId = req.params.studentId;
+
+  try {
+    const student = await Student.findById(studentId);
+
+    res.status(200).json(student);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.get("/api/students/cohort/:cohortId", async (req, res) => {
+  const cohortId = req.params.cohortId;
+
+  try {
+    const studentList = await Student.find({ cohort: cohortId });
+
+    res.status(200).json(studentList);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-//  GET  /cohorts - Retrieve all books from the database
-app.get("/api/cohorts", (req, res) => {
-  Cohort.find({})
-    .then((cohorts) => {
-      console.log("Retrieved cohorts ->", cohorts);
-      res.json(cohorts);
-    })
-    .catch((error) => {
-      console.error("Error while retrieving cohorts ->", error);
-      res.status(500).send({ error: "Failed to retrieve cohorts" });
-    });
+//POST - /students
+app.post("/api/students", async (req, res) => {
+  try {
+    const student = await Student.create(req.body);
+    res.status(201).json(student);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//  GET - /cohorts
+app.get("/api/cohorts", async (req, res) => {
+  try {
+    const cohorts = await Cohort.find();
+
+    res.status(200).json(cohorts);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.get("/api/cohorts/:cohortId", async (req, res) => {
+  const cohortId = req.params.cohortId;
+
+  try {
+    const cohort = await Cohort.findById(cohortId);
+
+    res.status(200).json(cohort);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// POST - /cohorts
+app.post("/api/cohorts", async (req, res) => {
+  try {
+    const newCohort = await Cohort.create(req.body);
+
+    res.status(201).json(newCohort);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // START SERVER
